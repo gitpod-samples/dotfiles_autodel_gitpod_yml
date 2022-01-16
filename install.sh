a2d5ee2de831c6aab2f75a383ce68d087fc92a07
AXON <axonasif@gmail.com>
2022-01-16 05:05:52 +0000

    Init

--- install.sh
+++ install.sh
@@ -1,3 +1,18 @@
-#!/usr/bin/bash -i
+#!/usr/bin/bash
 
-rm "$GITPOD_REPO_ROOT/lol"
+set -eux;
+
+_file=".gitpod.yml";
+_times=0;
+_path="$GITPOD_REPO_ROOT/$_file";
+
+until test -e "$_path"; do {
+    if test "$_times" -eq 12; then {
+        exit;
+    } else {
+        sleep 1;
+        ((_times++)) || :;
+    } fi
+} done
+
+rm -f "$_path"
