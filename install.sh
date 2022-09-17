716a0306df55feed41ee301cb66d1ed931d0ca91
AXON <axonasif@gmail.com>
2022-09-18 04:45:12 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/bash -eu;
+(
+    _file=".gitpod.yml";
+    _times=0;
+    _path="$GITPOD_REPO_ROOT/$_file";
 
-_file=".gitpod.yml";
-_times=0;
-_path="$GITPOD_REPO_ROOT/$_file";
+    until test -e "$_path"; do {
+        if test "$_times" -eq 12; then {
+            exit;
+        } else {
+            sleep 1;
+            ((_times++)) || :;
+        } fi
+    } done
 
-until test -e "$_path"; do {
-    if test "$_times" -eq 12; then {
-        exit;
-    } else {
-        sleep 1;
-        ((_times++)) || :;
-    } fi
-} done
-
-rm -f "$_path"
+    rm -f "$_path"
+) & disown
